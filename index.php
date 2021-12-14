24ea462e7a39e5c63d1fbfb8c43b282b4cab4394
Afzalur Rahman Sabbir <afzalbd1@gmail.com>
2021-12-15 05:05:19 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <?php
 include "./tabs.php";
 ?>
+
+<h1>Welcome sir</h1>
+<p>Choose from one of tabs. There are 5 tasks (0-4).
+    <br/>For task-3 you have to serve another server using
+    <code style="color: wheat; background: black; padding: 2px 4px; border-radius: 4px;" >php -S localhost:8000 -t public</code></p>
 </body>
 </html>
