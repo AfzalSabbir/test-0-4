c336eeb4a00a9b3fe4cdb84468246aceedd35984
Afzalur Rahman Sabbir <afzalbd1@gmail.com>
2021-12-15 05:09:59 +0600

    Update task-1.php

--- task-1/task-1.php
+++ task-1/task-1.php
@@ -1,4 +1,6 @@
 <strong>Q. What are your goals for the future?</strong>
 </br>
-<p>A. Currently I'm working with Laravel & VueJs. First I want to gather deep knowledge about these 2 technology and
-    besides this I want to learn NodeJs, Python & ReactJs.</p>
+<p>A. Currently, I'm working with Laravel & VueJs.<br/>My wish is to gather deep knowledge about these 2 technology and
+    beside them, I want to learn NodeJs, Python & ReactJs.<br>I want to develop my team lead skill to be a project
+    manager.
+</p>
